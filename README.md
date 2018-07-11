d7d0e37191730ea2058ff7487768a8da1c105a70
z3roblock <benwang1209@icloud.com>
2018-07-12 03:01:24 +0800

    0.2 更新，所有版面支持，bypass 18only 頁面。

--- README.md
+++ README.md
@@ -7,8 +7,21 @@ ptt.cc beauty board image downloader written in python 3+
 
 `config.ini` 內的設定跑掉或少行會自動修正  
 ```
-location: 'D:\Beauty' (def) 儲存文件的位置  
-threads: 10 (def) 訪問文章的線程數  
-temp_page: 0 (def)記憶用，第二次執行時直接從記錄的頁面開始  
-end_page: 1200 (def)必須比 temp_page 小，否則報錯，到達頁數後停止爬圖  
+location: 'D:\\' (def) 
+儲存文件的根目錄，下方設置的版面將在目錄下生成目錄 ie 'D:\root\path\Beauty'  
+
+threads: 10 (def) 
+訪問文章的線程數  
+
+temp_page: 0 (def)
+記憶用，第二次執行時直接從記錄的頁面開始  
+
+end_page: 1200 (def)
+必須比 temp_page 小，否則報錯，到達頁數後停止爬圖 
+
+filter: '公告,男,帥哥,鮮肉' 
+標題內容排除關鍵字，用半型 ',' 隔開
+
+board: 'Beauty' 
+預設版面，順從 https://www.ptt.cc/bbs/{}/ 的網址，限制級 bypass 內置。
 ```
--- get_beauty.py
+++ get_beauty.py
@@ -5,7 +5,7 @@ from logging.handlers import TimedRotatingFileHandler
 import time, os, queue, threading, chardet, re, requests, logging, configparser
 
 INTRO = '''
-COPYRIGHT 2018              PTT Beauty 爬圖工具
+COPYRIGHT 2018                     PTT 爬圖工具
       ____        ___    __ __  ___       _    
      |___ \      / _ \  / //_ |/ _ \     | |   
   ____ __) |_ __| | | |/ /_ | | | | | ___| | __
@@ -16,12 +16,16 @@ COPYRIGHT 2018              PTT Beauty 爬圖工具
 @theburger91                github.com/limeless                                                   
     '''
 
-DEF_SECTION = 'Beauty'
+DEF_SECTION = 'Config'
 DEF_THDS = '10'
-DEF_LOC = 'D:\Beauty'
+DEF_LOC = "D:\\"
 DEF_ENPP = '1200'
 DEF_TMPP = '0'
-BEAUTY_URL = 'https://www.ptt.cc/bbs/Beauty/index.html'
+DEF_URL = 'https://www.ptt.cc/bbs/'
+DEF_BOARD = 'Beauty'
+DEF_FILTER = '公告,男,帥哥,鮮肉'
+DEF_TITLE_HEAD = ['Re: ', 'Fw: ']
+
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 
 class myThread(threading.Thread):
@@ -31,9 +35,9 @@ class myThread(threading.Thread):
         self.name = name
         self.q = q
     def run(self):
-        logging.debug("開始線程 : " + self.name)
+        logging.debug(" 開始線程 : " + self.name)
         process_data(self.name, self.q)
-        logging.debug("結束線程 : " + self.name)
+        logging.debug(" 結束線程 : " + self.name)
 
 def process_data(threadName, q):
     while not exitFlag:
@@ -44,22 +48,21 @@ def process_data(threadName, q):
             url = temp_list[0]
             title = temp_list[1]
             useragent = temp_list[2]
-            if 'Re:' in title:
-                title = title.replace('Re: ', 'Re_')
-                logging.info('文章標題處理: {}'.format(title))
-            elif 'Fw:' in title:
-                title = title.replace('Fw: ', 'Fw_')
-                logging.info('文章標題處理: {}'.format(title))
-            res = requests.get(url, headers=useragent)
+            res = requests.get(url, headers=useragent, cookies={'over18': '1'})
             soup = BeautifulSoup(res.text, 'lxml')
             if len(soup.findAll('a', {'href':re.compile('http:\/\/i\.imgur\.com\/.*')})) > 0:
+                for x in DEF_TITLE_HEAD:
+                    if x in title:
+                        title = title.replace(x[2:4], '_')
+                        logging.info(' 文章標題處理: {}'.format(title))
                 logging.info('{}: 正在 {} 尋找圖檔……'.format(threadName, title))
                 for index, img_url in enumerate(soup.findAll('a', {'href':re.compile('http:\/\/i\.imgur\.com\/.*')})):
                     try:
-                        urlretrieve(img_url['href'], '{}\{}_{}.jpg'.format(loc, title, index))
+                        urlretrieve(img_url['href'], '{}{}\\{}_{}.jpg'.format(loc, board, title, index))
                         logging.debug('{}: {} {}_{}.jpg 下載成功!'.format(threadName, img_url['href'], title, index))
                     except:
                         logging.debug('{}: {} {}_{}.jpg 下載失敗!'.format(threadName, img_url['href'], title, index))
+                        logging.debug('{}{}\\{}_{}.jpg'.format(loc, board, title, index))
                     
                     time.sleep(0.1)
         else:
@@ -67,12 +70,15 @@ def process_data(threadName, q):
         
 
 def get_latest_page(url, ua):
-    res = requests.get(url, headers=ua)
+    url = url + board + '/index.html'
+    logging.debug(' requesting page: {}'.format(url))
+    res = requests.get(url, headers=ua, cookies={'over18': '1'})
     soup = BeautifulSoup(res.text, 'lxml')
     for btn in soup.select('.action-bar a'):
         if '上頁' in btn.text:
             pagenum = re.findall( r'\d+', btn.get('href'))
             latest_page = int(pagenum[0]) + 1
+            logging.debug(' Latest Page Number is :{}'.format(latest_page))
             return latest_page
 
 
@@ -80,12 +86,15 @@ def gencfg():
     cp.add_section(DEF_SECTION)
     section = cp[DEF_SECTION]
     section['location'] = DEF_LOC
+    section['board'] = DEF_BOARD
     section['threads'] = DEF_THDS
     section['temp_page'] = DEF_TMPP
     section['end_page'] = DEF_ENPP
+    section['filter'] = DEF_FILTER
     with open('config.ini', 'w') as f:
         cp.write(f)
     return
+
 
 def getConfig(setting_key, default_value):
     value = None
@@ -95,7 +104,7 @@ def getConfig(setting_key, default_value):
         cp[DEF_SECTION][setting_key] = default_value
         with open('config.ini', 'w') as f:
            cp.write(f)
-        logging.warn('選項遺失, 正在重建... {}: {}'.format(setting_key, default_value))
+        logging.warn(' 選項遺失, 正在重建... {}: {}'.format(setting_key, default_value))
         return getConfig(setting_key, default_value)
     return value
 
@@ -112,17 +121,26 @@ def setlog():
         root.addHandler(hdlr)
         root.setLevel(level)
         ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
+        ch.setLevel(logging.DEBUG)
         ch.setFormatter(fmt)
         root.addHandler(ch)
     return
 
 
+def filter_title(title):
+    for x in filt:
+        if x in title:
+            logging.info('條件符合, 跳過 {}'.format(title))
+            return None
+        else:
+            return title
+    
+
 if __name__ == '__main__':
     print(INTRO)
     time.sleep(2.0)
     setlog()
-    cp = configparser.ConfigParser(allow_no_value=True)
+    cp = configparser.ConfigParser()
     try:
         cp.read('config.ini')
     except Exception as e:
@@ -136,21 +154,24 @@ if __name__ == '__main__':
         cp.sections()
     except:
         cp.add_section(DEF_SECTION)
-    
+
     loc = getConfig('location', DEF_LOC)
+    board = getConfig('board', DEF_BOARD)
     thds = int(getConfig('threads', DEF_THDS))
     endp = int(getConfig('end_page', DEF_ENPP))
     tmpp = int(getConfig('temp_page', DEF_TMPP))
+    filt = getConfig('filter', DEF_FILTER).split(',')
+    logging.debug(filt)
 
-    if not os.path.exists(loc):
-        os.makedirs(loc)
+    if not os.path.exists(loc+'\\'+board):
+        os.makedirs(loc+'\\'+board)
     
-    logging.debug('讀取 User-Agent 資料...')    
+    logging.debug(' 讀取 User-Agent 資料...')    
     ua = UserAgent()
     useragent = {'User-Agent': ua.random}
 
     if tmpp == 0:
-        pagenum = get_latest_page(BEAUTY_URL, useragent)
+        pagenum = get_latest_page(DEF_URL, useragent)
     else:
         pagenum = tmpp
 
@@ -173,48 +194,44 @@ if __name__ == '__main__':
         
         queueLock.acquire()
         logging.info('目前頁面: {}'.format(pagenum))
-        curres = 'https://www.ptt.cc/bbs/Beauty/index{}.html'.format(pagenum)
+        curres = 'https://www.ptt.cc/bbs/{}/index{}.html'.format(board, pagenum)
         useragent = {'User-Agent': ua.random}
         logging.debug('UA:{}'.format(useragent))
-        res = requests.get(curres, headers=useragent)
+        res = requests.get(curres, headers=useragent, cookies={'over18': '1'})
         soup = BeautifulSoup(res.text, 'lxml')
-        logging.info('Loading page: {}'.format(curres))
+        logging.info(' Loading page: {}'.format(curres))
         for article in soup.select('.r-ent a'):
             temp_list = []
             url = 'https://www.ptt.cc' + article['href']
             title = article.text
+            
             if 'search' in url:
                 pass
-            elif '[帥哥]' in title:
-                logging.info('發現非正妹, 跳過! {}'.format(title))
-                pass
-            elif '鲜肉' in title:
-                logging.info('發現非正妹, 跳過! {}'.format(title))
-                pass
-            elif '男' in title:
-                logging.info('發現非正妹, 跳過! {}'.format(title))
-                pass
-            elif '公告' in title:
-                logging.info('發現非正妹, 跳過! {}'.format(title))
+
+            elif filter_title(title) == None:
                 pass
             else:
                 temp_list.append(url)
                 temp_list.append(title)
                 temp_list.append(useragent)
                 workQueue.put(temp_list)
+
         queueLock.release()
 
         while not workQueue.empty():
             pass
-        
+
         exitFlag = 1
-        
+
         for t in threads:
             t.join()
-        logging.info('DONE: {}'.format(curres))
+
+        logging.info(' DONE: {}'.format(curres))
         pagenum -= 1
-        cp.set('Beauty', 'temp_page', str(pagenum))
+
+        cp.set(DEF_SECTION, 'temp_page', str(pagenum))
         with open('config.ini', 'w') as f:
             cp.write(f)
+
         time.sleep(0.2)
-    logging.info('ALL DONE')
+    logging.info(' ALL DONE')
